3856cdd8224a0ea7cfe142cbdbff20fd3589d5c2
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2019-05-23 16:53:00 +0200

    Grywalna wersja

--- Saper/Saper/Program.fs
+++ Saper/Saper/Program.fs
@@ -3,7 +3,9 @@
 open System
 
 let closed_field_char = '#'
+let mine_field_char = 'M'
 let mine_field_value = -1
+let third (_, _, c) = c
 
 let rec get_int_in_range_from_input communicate min max =
     let get_int_from_input comunicate default_eror_value =
@@ -95,6 +97,16 @@ let choose_game_variant =
     let game_variant = get_int_in_range_from_input "Wybierz wariant gry od 1 do 3" 1 3
     game_variants.Item(game_variant - 1)
 
+let do_for_neighbours board user_board x y f =
+    f board user_board (x - 1) (y - 1)
+    f board user_board (x - 1) y
+    f board user_board (x - 1) (y + 1)
+    f board user_board x (y - 1)
+    f board user_board x (y + 1)
+    f board user_board (x + 1) (y - 1)
+    f board user_board (x + 1) y
+    f board user_board (x + 1) (y + 1)
+
 let rec guess board user_board x y =
     if (x >= 0 && x < Array2D.length1 board && y >= 0 && y < Array2D.length2 board 
     && Array2D.get user_board x y = closed_field_char) then
@@ -103,21 +115,50 @@ let rec guess board user_board x y =
         else
             user_board.SetValue((char (Array2D.get board x y) + '0'), x, y)
             if (Array2D.get board x y = 0) then
-                guess board user_board (x - 1) (y - 1)
-                guess board user_board (x - 1) y
-                guess board user_board (x - 1) (y + 1)
-                guess board user_board x (y - 1)
-                guess board user_board x (y + 1)
-                guess board user_board (x + 1) (y - 1)
-                guess board user_board (x + 1) y
-                guess board user_board (x + 1) (y + 1)
+                do_for_neighbours board user_board x y guess
 
+let uncover_neighbours board user_board x y = 
+    if (x >= 0 && x < Array2D.length1 board && y >= 0 && y < Array2D.length2 board 
+    && Array2D.get user_board x y <> closed_field_char) then
+        do_for_neighbours board user_board x y guess
+
+let sign_as_mine _ user_board x y =
+    if (x >= 0 && x < Array2D.length1 user_board && y >= 0 && y < Array2D.length2 user_board 
+    && Array2D.get user_board x y = closed_field_char) then
+        user_board.SetValue(mine_field_char, x, y)
+
+let sign_as_not_mine _ user_board x y =
+    if (x >= 0 && x < Array2D.length1 user_board && y >= 0 && y < Array2D.length2 user_board 
+    && Array2D.get user_board x y = mine_field_char) then
+        user_board.SetValue(closed_field_char, x, y)
 
 let get_point_from_user max_x max_y =
     let x = get_int_in_range_from_input "wprowadź x" 0 max_x
     let y = get_int_in_range_from_input "wprowadź y" 0 max_y
     (x, y)
 
+let choose_methode methodes communicate min_id max_id =
+    let methode_id = get_int_in_range_from_input communicate min_id max_id
+    List.item (methode_id - 1) methodes
+
+let is_winner user_board amount_of_mines =
+    let count_open_fields user_board = 
+         let rec _count_open_fields user_board accum index = 
+            if (index = Array2D.length1 user_board * Array2D.length2 user_board) then
+                accum
+            else
+                let x = index % (Array2D.length1 user_board)
+                let y = index / (Array2D.length1 user_board)
+
+                if (Char.IsDigit user_board.[x, y]) then
+                    _count_open_fields user_board (accum + 1) (index + 1)
+                else
+                    _count_open_fields user_board accum (index + 1)
+         in
+            _count_open_fields user_board 0 0
+    in
+        (Array2D.length1 user_board * Array2D.length2 user_board) - amount_of_mines = count_open_fields user_board
+    
 
 [<EntryPoint>]
 let main argv =
@@ -125,18 +166,24 @@ let main argv =
     let variant = choose_game_variant
     let board = generate_board variant
     let user_board = Array2D.create<char> (Array2D.length1 board) (Array2D.length2 board) closed_field_char
+    let methodes = [guess; sign_as_mine; sign_as_not_mine; uncover_neighbours]
 
     print_user_table user_board
 
     try
-        while (true) do
+        while (not (is_winner user_board (third variant))) do
+            let methode = choose_methode methodes "Odkryj pole (1) / Oznacz minę (2) / Odznacz minę(3) / Odkryj sąsiednie (4)" 1 4
             let point = get_point_from_user (Array2D.length1 board) (Array2D.length2 board)
             Console.Clear()
-            guess board user_board (fst point) (snd point)
+            methode board user_board (fst point) (snd point)
             print_user_table user_board
+
+        printfn "Wygrałeś! Gratulacje!"
+
     with
         _ -> 
             Console.Clear()
             printfn "Przegrałeś! Wszedłeś na minę"
-            print_mines_table board
+    
+    print_mines_table board
     0
